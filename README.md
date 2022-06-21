dae31d41695da07ff872fdfef64d3ffd4d3a3ad0
lilotimo <lilt33mo@gmail.com>
2022-06-20 22:25:50 -0300

    add swap button; fix functions to clean dom

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ decimalbinaryconverter.com
 
 - consertar o algoritmo de conversão entre bases
 - colocar o botão de swap para alternar as bases
+- aprimorar o algortimo da matrix, tá dando lag
--- src/js/dom-changes.js
+++ src/js/dom-changes.js
@@ -9,76 +9,33 @@ var outputLabel = document.querySelector('#output-label');
 
 var convertButton = document.querySelector('#convert-button');
 var resetButton = document.querySelector('#reset-button');
+var swapButton = document.querySelector('#swap-button');
 
-// Event listeners
+// Event listeners do buttons
 
 fromSelect.addEventListener('change', () => {
-  inputLabel.innerHTML = `Enter ${fromSelect.value} number`;
+  checkLabels();
 
-  if (fromSelect.value === 'decimal') input.placeholder = 'Only numbers from 0 to 9';
-
-  if (fromSelect.value === 'binary') input.placeholder = 'Only numbers 0 and 1';
-
-  if (fromSelect.value === 'octal') input.placeholder = 'Only numbers from 0 to 7';
-
-  if (fromSelect.value === 'hexadecimal') input.placeholder = 'Only numbers from 0 to 9 and letters from "a" to "e" or "A" to "E"';
-
-  if (fromSelect.value === 'text') input.placeholder = 'Enter a text';
+  checkPlaceholders();
 
   checkInput(input);
 });
 
-toSelect.addEventListener('change', (e) => {
-  outputLabel.innerHTML = `${e.target.value[0].toUpperCase() + e.target.value.slice(1)} ${e.target.value === 'text' ? '' : 'number'}`;
-});
+toSelect.addEventListener('change', (e) => checkLabels);
 
 input.addEventListener('keyup', (e) => checkInput(e.target));
 
 convertButton.onclick = () => outputConvertedNumber();
 
-resetButton.onclick = () => cleanInputAndOutput();
+resetButton.onclick = () => {
+  cleanInput();
 
-// Funções auxiliares
+  cleanOutput();
+};
 
-function checkInput(element) {
-  const target = element;
-  const val = element.value;
+swapButton.onclick = () => swapSelectsValues();
 
-  const setBackgroundAndButtonToNormalState = () => {
-    target.style.backgroundColor = '#fdfcfa';
-    convertButton.disabled = false;
-  };
-
-  if (fromSelect.value === 'decimal') {
-    if (!/^[0-9]+$/.test(val) && val.length > 0) {
-      target.style.backgroundColor = '#ff4c3033';
-      convertButton.disabled = true;
-    } else setBackgroundAndButtonToNormalState();
-  }
-
-  if (fromSelect.value === 'binary') {
-    if (!/^[0-1]+$/.test(val) && val.length > 0) {
-      target.style.backgroundColor = '#ff4c3033';
-      convertButton.disabled = true;
-    } else setBackgroundAndButtonToNormalState();
-  }
-
-  if (fromSelect.value === 'octal') {
-    if (!/^[0-7]+$/.test(val) && val.length > 0) {
-      target.style.backgroundColor = '#ff4c3033';
-      convertButton.disabled = true;
-    } else setBackgroundAndButtonToNormalState();
-  }
-
-  if (fromSelect.value === 'hexadecimal') {
-    if (!/^[0-9a-fA-F]+$/.test(val) && val.length > 0) {
-      target.style.backgroundColor = '#ff4c3033';
-      convertButton.disabled = true;
-    } else setBackgroundAndButtonToNormalState();
-  }
-
-  if (fromSelect.value === 'text') setBackgroundAndButtonToNormalState();
-}
+// Funções auxiliares de cálculo
 
 // Não funciona para números muito grandes, refazer usando
 // o algoritmo matemático
@@ -120,12 +77,90 @@ function outputConvertedNumber() {
 
   if (toSelectValue === fromSelectValue) output.innerHTML = inputValue;
 
-  if (!inputValue) cleanInputAndOutput();
+  if (!inputValue) {
+    cleanInput();
+    cleanOutput();
+  }
 }
 
-function cleanInputAndOutput() {
-  input.value = '';
+// Funções auxiliares de manipulação do DOM
+
+function swapSelectsValues() {
+  const fromSelectValue = fromSelect.value;
+  const toSelectValue = toSelect.value;
+
+  fromSelect.value = toSelectValue;
+  toSelect.value = fromSelectValue;
+
+  checkInput(input);
+}
+
+function checkInput(element) {
+  const target = element;
+  const val = element.value;
+
+  const setBackgroundAndButtonToNormalState = () => {
+    target.style.backgroundColor = '#fdfcfa';
+    convertButton.disabled = false;
+  };
+
+  if (fromSelect.value === 'decimal') {
+    if (!/^[0-9]+$/.test(val) && val.length > 0) {
+      target.style.backgroundColor = '#ff4c3033';
+      convertButton.disabled = true;
+    } else setBackgroundAndButtonToNormalState();
+  }
+
+  if (fromSelect.value === 'binary') {
+    if (!/^[0-1]+$/.test(val) && val.length > 0) {
+      target.style.backgroundColor = '#ff4c3033';
+      convertButton.disabled = true;
+    } else setBackgroundAndButtonToNormalState();
+  }
+
+  if (fromSelect.value === 'octal') {
+    if (!/^[0-7]+$/.test(val) && val.length > 0) {
+      target.style.backgroundColor = '#ff4c3033';
+      convertButton.disabled = true;
+    } else setBackgroundAndButtonToNormalState();
+  }
+
+  if (fromSelect.value === 'hexadecimal') {
+    if (!/^[0-9a-fA-F]+$/.test(val) && val.length > 0) {
+      target.style.backgroundColor = '#ff4c3033';
+      convertButton.disabled = true;
+    } else setBackgroundAndButtonToNormalState();
+  }
+
+  if (fromSelect.value === 'text') setBackgroundAndButtonToNormalState();
+}
+
+function checkPlaceholders() {
+  if (fromSelect.value === 'decimal') input.placeholder = 'Only numbers from 0 to 9';
+
+  if (fromSelect.value === 'binary') input.placeholder = 'Only numbers 0 and 1';
+
+  if (fromSelect.value === 'octal') input.placeholder = 'Only numbers from 0 to 7';
+
+  if (fromSelect.value === 'hexadecimal') input.placeholder = 'Only numbers from 0 to 9 and letters from "a" to "e" or "A" to "E"';
+
+  if (fromSelect.value === 'text') input.placeholder = 'Enter a text';
+}
+
+function checkLabels() {
+  inputLabel.innerHTML = `Enter ${fromSelect.value} number`;
+
+  outputLabel.innerHTML = `${e.target.value[0].toUpperCase() + e.target.value.slice(1)} ${e.target.value === 'text' ? '' : 'number'}`;
+}
+
+function cleanOutput() {
   output.innerHTML = '';
+
+  checkInput(input);
+}
+
+function cleanInput() {
+  input.value = '';
 
   checkInput(input);
 }
